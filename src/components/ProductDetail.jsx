175fac17f5bd3b10a97c37948b3557d5828df932
nomanvaghela <nomanvaghela7@gmail.com>
2024-04-07 15:35:41 +0530

    card icon add in products details page

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -9,7 +9,7 @@ const ProductDetail = () => {
   console.log(product);
   return (
     <div>
-      <div className=" bg-gray-100 dark:bg-gray-800 py-8">
+      <div className=" bg-gray-100 py-8">
         <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex flex-col md:flex-row -mx-4">
             <div className="md:flex-1 px-4">
@@ -24,7 +24,20 @@ const ProductDetail = () => {
                 <div className="w-full px-2">
                   <button
                     onClick={() => dispatch(addToCart(product))}
-                    className="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">
+                    className="w-full bg-gray-900 flex justify-center text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="mr-2 h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}>
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                      />
+                    </svg>
                     Add to Cart
                   </button>
                 </div>
