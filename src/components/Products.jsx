aad5d63df11c963b2a57e55ab7ae9700c39791f1
nomanvaghela <nomanvaghela7@gmail.com>
2024-04-09 15:48:20 +0530

    asa

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getData } from "../redux/productsSlice";
 import { addToCart } from "../redux/productsSlice";
