327d70b3cf1cee640ebdce2259edd67c06ff700f
nomanvaghela <nomanvaghela7@gmail.com>
2024-06-05 14:46:23 +0530

    sdsds

--- src/components/ratting/CategoryFilter.jsx
+++ src/components/ratting/CategoryFilter.jsx
@@ -14,21 +14,23 @@ const CategoryFilter = () => {
   const dispatch = useDispatch();
 
   return (
-    <div className="flex items-center justify-center flex-col container mx-auto px-4">
-      <div className="flex justify-center items-center mt-5">
-        {buttons.map((button, i) => {
-          return (
-            <button
-              key={i}
-              id="dropdownDefault"
-              data-dropdown-toggle="dropdown"
-              onClick={() => dispatch(filterCategory(button))}
-              className=" border-1 border-black p-2 ring-1 ring-gray-300 me-3 focus:ring-4 focus:bg-gray-800 focus:text-white focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center items-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              type="button">
-              {button}
-            </button>
-          );
-        })}
+    <div className="container mx-auto px-4 mt-5">
+      <div className="flex items-center justify-center flex-col flex-wrap px-4  mx-auto">
+        <div className="mx-auto">
+          {buttons.map((button, i) => {
+            return (
+              <button
+                key={i}
+                id="dropdownDefault"
+                data-dropdown-toggle="dropdown"
+                onClick={() => dispatch(filterCategory(button))}
+                className=" border-1 border-black p-2 ring-1 ring-gray-300 m-2 focus:ring-4 focus:bg-gray-800 focus:text-white focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center items-center"
+                type="button">
+                {button}
+              </button>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
